d1ea48d852b1098bc8c5bbd6883f13782de0a9bf
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2020-03-24 22:34:53 -0500

    commit

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const http = require('http');
+const http = require('http') // require built-in Node.js http package
 
-const hostname = '127.0.0.3';
-const port = 7007;
+// Use hosting values if available, otherwise default 
+const hostname = process.env.hostname || '0.0.0.0' // allow remote access
+const port = process.env.PORT || 3002
 
+// define our server
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, This is Pruthvi Naskanti');
-});
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello, this is Pruthvi')
+})
 
-
+// start listening
+// use the server console to tell user where to find the server
+// use backticks for template literals with embedded expressions
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
